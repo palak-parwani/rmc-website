17725a4c4e8c5ade0b657b758eddb4e6cdb70f5d
palak-parwani <parwanipalak3@gmail.com>
2025-12-28 15:40:19 +0530

    add animation in banner

--- src/components/HeroBanner/HeroBanner.jsx
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -10,31 +10,33 @@ export default function HeroBanner() {
         <Row className={`align-items-center ${styles.padding}`}>
           {/* LEFT CONTENT */}
           <Col md={7}>
-            <h1 className={styles.title}>
-              Premium Ready-Mix Concrete for Strong & Reliable Construction
-            </h1>
+            <div className={styles.textAnimate}>
+              <h1 className={styles.title}>
+                Premium Ready-Mix Concrete for Strong & Reliable Construction
+              </h1>
 
-            <p className={styles.subtitle}>
-              High-quality, precision-mixed concrete produced using advanced
-              batching technology. Assured strength, durability, and timely
-              delivery for residential, commercial, and infrastructure projects.
-            </p>
+              <p className={styles.subtitle}>
+                High-quality, precision-mixed concrete produced using advanced
+                batching technology. Assured strength, durability, and timely
+                delivery for residential, commercial, and infrastructure projects.
+              </p>
 
-            {/* CTA Buttons */}
-            <div className={styles.btnGroup}>
-              <a href=" /contactus">
-                <Button className={styles.primaryBtn}>
-                  Get a Free Quote
-                </Button>
-              </a>
+              <div className={styles.btnGroup}>
+                <a href=" /contactus">
+                  <Button className={styles.primaryBtn}>
+                    Get a Free Quote
+                  </Button>
+                </a>
 
-              <a href=" /products">
-                <Button className={styles.secondaryBtn}>
-                  Explore Mix Grades →
-                </Button>
-              </a>
+                <a href=" /products">
+                  <Button className={styles.secondaryBtn}>
+                    Explore Mix Grades →
+                  </Button>
+                </a>
+              </div>
             </div>
           </Col>
+
         </Row>
       </div>
     </section>
